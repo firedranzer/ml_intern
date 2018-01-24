48f20639a15138a52d58a414ce6b0a2d8c62513b
firedranzer <rupeshharode@gmail.com>
2018-01-24 16:12:08 +0530

    Modified build.py file

--- olympics-mini-project/build.py
+++ olympics-mini-project/build.py
@@ -1,12 +1,32 @@
 import pandas as pd
+import re
 
 
 def load_data():
-    """
-        Enter your code here
-    """
+    #Importing CSV file
+    df = pd.read_csv("./data/olympics.csv", header=1)
+    
+    #Substituting numbers with corresponding medals
+    df = df.rename(columns=lambda x : re.sub('^01 !', 'Gold', x))
+    df = df.rename(columns=lambda x : re.sub('^02 !', 'Siver', x))
+    df = df.rename(columns=lambda x : re.sub('^03 !', 'Bronze', x))
+    
+    #Make seperate list of country names and codes.
+    country_name, country_code = []
+    for i in df['Unnamed: 0']:
+        country_name.append(i[0:i.find('(')].replace(u'\xa0', u''))
+        country_code.append(i[i.find('(')+1: i.find(')')])
 
+    #Inserting updated rows
+    del df['Unnamed: 0']
+    df.insert(0, 'country_name', country_name)
+    df.insert(1, 'country_code', country_code)
+    df = df.set_index('country_name')
 
+    #drop Totals column
+    df.drop(df.tail(1).index, inplace=True)
+    return df
+    
 def first_country(df):
     """
         Enter your code here
