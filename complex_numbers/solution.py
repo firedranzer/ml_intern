6f832cd5dade4fbd4612655e9cc49fded55750d4
firedranzer <rupeshharode@gmail.com>
2018-01-24 23:25:38 +0530

    Minor Changes

--- complex_numbers/solution.py
+++ complex_numbers/solution.py
@@ -1,27 +1,28 @@
 from math import pow
-class complex_number:
-	
+
+class complex:
+    	
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 	
 	def __add__(self, next):
-		return complex_number(self.x+next.x, self.y+next.y)
+		return complex(self.x+next.x, self.y+next.y)
 	
 	def __sub__(self, next):
-		return complex_number(self.x-next.x, self.y-next.y)
+		return complex(self.x-next.x, self.y-next.y)
 	
 	def __mul__(self, next):
-		return complex_number(self.x*next.x-self.y*next.y, self.x*next.y+self.y*next.x)
+		return complex(self.x*next.x-self.y*next.y, self.x*next.y+self.y*next.x)
 	
 	def __truediv__(self, next):
 		try: 
-			return self.__mul__(complex_number(next.x, -1*next.y)).__mul__(complex_number(1.0/(next.mod().x)**2, 0))
+			return self.__mul__(complex(next.x, -1*next.y)).__mul__(complex(1.0/(next.mod().x)**2, 0))
 		except ZeroDivisionError as e:
 			print (e)
 			return None
 	def mod(self):
-		return complex_number(pow(self.x**2+self.y**2, 0.5), 0)
+		return complex(pow(self.x**2+self.y**2, 0.5), 0)
 	
 	def __str__(self, precision=2):
 		if self.x==int(self.x) and self.y==int(self.y):	
@@ -40,14 +41,15 @@ class complex_number:
 
 	def conjugate(self):
 		if self.y > 0:
-			return complex_number(self.x,-1*self.y)
+			return complex(self.x,-1*self.y)
 		else:
-			return complex_number(self.x,self.y)
+			return complex(self.x,self.y)
+
 
 a,b = input().strip().split()
 x,y = input().strip().split()
-A = complex_number(float(a),float(b))
-B = complex_number(float(x),float(y))
+A = complex(float(a),float(b))
+B = complex(float(x),float(y))
 
 
 print (A+B)
